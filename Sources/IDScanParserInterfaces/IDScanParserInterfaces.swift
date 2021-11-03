4a2f5134945ff4187043f9b6f42781b87583cb12
Alexander Korotkov <akorotkov@abyka.com>
2021-11-03 11:37:14 +0500

    fix

--- Sources/IDScanParserInterfaces/IDScanParserInterfaces.swift
+++ Sources/IDScanParserInterfaces/IDScanParserInterfaces.swift
@@ -2,5 +2,5 @@ import Foundation
 
 public protocol IIDScanParser {
     func parse(_ rawString: String) -> [String : String]?
-    static var version: String { get }
+    var version: String { get }
 }
