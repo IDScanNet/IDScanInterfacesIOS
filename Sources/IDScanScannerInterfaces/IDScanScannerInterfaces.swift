74b75bb3b76855809de684a43ea67055a668e2f3
Alexander Korotkov <akorotkov@abyka.com>
2022-01-19 17:52:41 +0500

    fixed detect funcs

--- Sources/IDScanScannerInterfaces/IDScanScannerInterfaces.swift
+++ Sources/IDScanScannerInterfaces/IDScanScannerInterfaces.swift
@@ -1,6 +1,8 @@
-import Foundation
+import CoreImage.CIImage
+import CoreMedia.CMSampleBuffer
 
 public protocol IIDScanScanner {
-    func detect(image: UnsafeMutablePointer<UInt8>, width: Int, height: Int) -> [String : Any]?
+    func detect(from: CIImage) -> [String : Any]?
+    func detect(from: CMSampleBuffer) -> [String : Any]?
     var version: String { get }
 }
