a6e2e9209b546d5746e96cdb4ca34f40a67d0c90
Alexander Korotkov <eiros@me.com>
2022-11-09 10:35:32 +0500

    Added IDScanIDScannerDelegate

--- Sources/IDScanScannerInterfaces/IDScanScannerInterfaces.swift
+++ Sources/IDScanScannerInterfaces/IDScanScannerInterfaces.swift
@@ -6,3 +6,8 @@ public protocol IIDScanScanner {
     func detect(from sampleBuffer: CMSampleBuffer) -> [String : Any]?
     var version: String { get }
 }
+
+public protocol IDScanIDScannerDelegate: AnyObject {
+    func idScanIDScannerResult(result: [String : Any], sender: AnyObject?)
+    func idScanIDScannerErrorOccured(error: Error, userInfo: [String : Any], sender: AnyObject?)
+}
